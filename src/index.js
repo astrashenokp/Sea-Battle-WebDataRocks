1cbabc747b63037dbabbefa1dea61cad7ece1f60
Polina Astrashenok <astrashenok225@gmail.com>
2026-06-02 20:44:59 +0300

    Add initial game client setup with status display

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css';
+
+console.log('Game client is running');
+
+const statusElement = document.getElementById('game-status');
+statusElement.innerText = 'Waiting for game settings...';
